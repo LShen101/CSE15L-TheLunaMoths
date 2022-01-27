0080e4f8d4f0c6ebd3284629fb842b4ceef729c5
Leica <s-shenl@bsd405.org>
2022-01-26 18:37:28 -0800

    test

---
+++ MarkdownParseTest.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+import java.beans.Transient;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition(){
+        assertEquals(2, 1 + 1);
+    }
+
+}
