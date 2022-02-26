c602a3eb192698ab661e5867c62a6ce8d0c6d82e
Leica <jis033@ucsd.edu>
2022-02-25 16:28:08 -0800

    .

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,3 +1,4 @@
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,7 +11,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while (currentIndex < markdown.length()) {
 
             System.out.println("MD Length: " + markdown.length());
             System.out.println("Current Index: " + currentIndex);
@@ -20,31 +21,30 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-		
-            // check if there are no [, ], (, or ) in the file 
+
+            // check if there are no [, ], (, or ) in the file
             if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
-		    return toReturn;
-	    }
+                return toReturn;
+            }
             if (nextCloseBracket + 1 == openParen) {
                 // check if either the link starts at index 0 OR has a "!" before it
-                if ((nextOpenBracket == 0 || 
-                    !(markdown.substring(nextOpenBracket - 1, nextOpenBracket).equals("!"))) && 
-                    //check if there re spaces in the link 
-                    markdown.substring(openParen + 1, closeParen).indexOf(" ") == -1) {
+                if ((nextOpenBracket == 0 ||
+                        !(markdown.substring(nextOpenBracket - 1, nextOpenBracket).equals("!"))) &&
+                // check if there re spaces in the link
+                        markdown.substring(openParen + 1, closeParen).indexOf(" ") == -1) {
                     toReturn.add(markdown.substring(openParen + 1, closeParen));
                 }
             }
             currentIndex = closeParen + 1;
         }
-	
+
         return toReturn;
     }
 
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
 }
-
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParseTest {
+    /*
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
@@ -109,6 +110,7 @@ public class MarkdownParseTest {
         expectedLinks.add("example.com");
         assertEquals(expectedLinks, links);
     }
+    */
 
     @Test
     public void testFile11() throws IOException{
